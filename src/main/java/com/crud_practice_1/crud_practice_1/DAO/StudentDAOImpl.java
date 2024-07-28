507bde1d74bd7c8627887260373ddc9d62339750
Angelina_Shoili_Rozario <shoili.rozario@gmail.com>
2024-07-28 03:08:59 -0600

    found out that update query does not return any result

--- src/main/java/com/crud_practice_1/crud_practice_1/DAO/StudentDAOImpl.java
+++ src/main/java/com/crud_practice_1/crud_practice_1/DAO/StudentDAOImpl.java
@@ -10,31 +10,32 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
 @Component
-public class StudentDAOImpl implements StudentDAO{
+public class StudentDAOImpl implements StudentDAO {
 
     // Initializing Entity Manager
     EntityManager entityManager;
+
     public StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
     // Using ENTITY MANAGER
-    //For creating one Student or many student
+    // For creating one Student or many student
     @Override
     @Transactional
     public void createEntityManager(String name, int age, double marks) {
-        Student student = new Student(name,age,marks);
+        Student student = new Student(name, age, marks);
         entityManager.persist(student);
-        System.out.println("Student has been added "+ student.toString());
+        System.out.println("Student has been added " + student.toString());
     }
+
     // X X X X Using JPQL
-    //there is no insert method in jpql 
+    // there is no insert method in jpql
     @Override
     @Transactional
     public void createJPQL(String name, int age, double marks) {
-        
+
     }
 
     // Using ENTITY MANAGER
@@ -42,8 +43,9 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public void readEntityManager(int id) {
         Student student = entityManager.find(Student.class, id);
-        System.out.println("Student Information with ID:  "+ student.toString());
+        System.out.println("Student Information with ID:  " + student.toString());
     }
+
     // Using JPQL
     // for reading all record or records following certain condition
     @Override
@@ -51,64 +53,65 @@ public class StudentDAOImpl implements StudentDAO{
         TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s", Student.class);
         List<Student> students = query.getResultList();
         System.out.println("Read all Student Informations");
-        for(Student student : students){
-            System.out.println("Student Information:  "+ student.toString());
+        for (Student student : students) {
+            System.out.println("Student Information:  " + student.toString());
         }
     }
-   
-    //UPDATE Entity Manager
+
+    // UPDATE Entity Manager
     // For updating a single record
-    //Search By ID
+    // Search By ID
     @Override
     @Transactional
     public void updateEntityManager(String name, int id) {
         Student student = entityManager.find(Student.class, id);
-        if(student != null){
+        if (student != null) {
             student.setName(name);
             entityManager.merge(student);
         }
-        System.out.println("The Name has been updated successfully. Details: "+student.toString());
+        System.out.println("The Name has been updated successfully. Details: " + student.toString());
     }
-    //UPDATE with JPQL
+
+    // UPDATE with JPQL
     // For updating multiple record for certain condition
-    //Search By Iteration
+    // Search By Iteration
     @Override
     @Transactional
     public void updateJPQL(String name, int id) {
-        System.out.println("Name "+name);
-         Query query = entityManager.createQuery("UPDATE Student s SET Name = :name WHERE s.id = :id");
-//        TypedQuery<Student> query = entityManager.createQuery("UPDATE Student s SET Name = :name WHERE s.id = :id", Student.class);
+        System.out.println("Name " + name);
+        Query query = entityManager.createQuery("UPDATE Student s SET Name = :name WHERE s.id = :id");
         query.setParameter("name", name);
         query.setParameter("id", id);
-        int success  = query.executeUpdate();
-        if(success > 0){
+        int success = query.executeUpdate();
+        
+        if (success > 0) {
             System.out.println("The Name has been updated successfully. ");
-        }else{
-            System.out.println("The Name has not been updated successfully.");
+        } else {
+            System.out.println("The Name has not been updated. ");
         }
     }
 
-    //DELETE : ENTITY MANAGER 
-    //GOOD FOR SINGLE ITEM DELETE
-    //DELETE By ID
+    // DELETE : ENTITY MANAGER
+    // GOOD FOR SINGLE ITEM DELETE
+    // DELETE By ID
     @Override
     @Transactional
     public void deleteEntityManager(int id) {
         Student student = entityManager.find(Student.class, id);
-        if(student!=null){
+        if (student != null) {
             entityManager.remove(student);
         }
     }
-     //DELETE : JPQL  
-     //When deleting multiple records with certain condition
-    //DELETE By Iteration
-     @Override
-     @Transactional
-     public void deleteJPQL(int id) {
-         Query theQuery = entityManager.createQuery("DELETE FROM Student s WHERE s.id = :id");
-         theQuery.setParameter("id", id);
-         theQuery.executeUpdate();
-     }
- 
-     
+
+    // DELETE : JPQL
+    // When deleting multiple records with certain condition
+    // DELETE By Iteration
+    @Override
+    @Transactional
+    public void deleteJPQL(int id) {
+        Query theQuery = entityManager.createQuery("DELETE FROM Student s WHERE s.id = :id");
+        theQuery.setParameter("id", id);
+        theQuery.executeUpdate();
+    }
+
 }
